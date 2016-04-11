77e1a2a1ea59a59f88007bfbff95ca7f6ecda048
Joris <b03902033@ntu.edu.tw>
2016-04-11 22:56:14 +0800

    add register activity and button

--- BasketStats/app/src/main/java/com/basketstats/basketstats/MainActivity.java
+++ BasketStats/app/src/main/java/com/basketstats/basketstats/MainActivity.java
@@ -1,13 +1,30 @@
 package com.basketstats.basketstats;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button newMatchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: show previous games
+
+        /*  Create a button for new page activity */
+        newMatchButton = (Button)findViewById(R.id.newMatch);
+        newMatchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // run register activity on click
+                Intent runRegisterActivity = new Intent(MainActivity.this, RegisterActivity.class);
+                MainActivity.this.startActivity(runRegisterActivity);
+            }
+        });
+
     }
 }
