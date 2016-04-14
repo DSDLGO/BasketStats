5d24543aa20313af81007815ded1e1e527166a27
whsunset <no.12586269025@gmail.com>
2016-04-15 01:28:09 +0800

    add some preprocess to game panel

--- BasketStats/app/src/main/java/com/basketstats/basketstats/statRecord.java
+++ BasketStats/app/src/main/java/com/basketstats/basketstats/statRecord.java
@@ -1,57 +1,73 @@
 package com.basketstats.basketstats;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import java.util.ArrayList;
-
-import static android.view.ViewGroup.LayoutParams.FILL_PARENT;
-import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-import static android.widget.LinearLayout.VERTICAL;
 
 public class statRecord extends AppCompatActivity {
 
     private ArrayList<String> playerList = new ArrayList<String>();
+    private String home_team, away_team, date;
+    private int home_score = 0, away_score = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stat_record);
 
         /* get players name from register activity */
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         int numOfPlayers = Integer.parseInt(extras.getString("numOfPlayers"));
-        for(int i = 0; i < numOfPlayers; i++)
-        {
+        home_team = extras.getString("team");
+        away_team = extras.getString("opp");
+        date = extras.getString("date");
+
+        for (int i = 0; i < numOfPlayers; i++) {
             String playerName = extras.getString(String.valueOf(i));
             playerList.add(playerName);
         }
 
-        /* one can access to registered name in "playerList" */
-
-
-        /********************below code is for test ************************/
-        /* set up layout for rendering players */
-        LinearLayout playerLayout = new LinearLayout(this);
-        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(FILL_PARENT, WRAP_CONTENT);
-        playerLayout.setLayoutParams(params);
-        playerLayout.setOrientation(VERTICAL);
-
-        /* render players name */
-        for(int i = 0; i < playerList.size(); i++)
-            playerLayout.addView(typeLabel(playerList.get(i), i));
-        setContentView(playerLayout);
-
+        processViews();
     }
 
-    private TextView typeLabel (String label, int id) {
-        TextView view = new TextView(this);
-        view.setId(id);
-        view.setTextSize(20);
-        view.setText(label);
-        return view;
+    private void processViews(){
+
+        Button homeTeamButton = (Button) findViewById(R.id.button_home_team);
+        homeTeamButton.setText(home_team);
+
+        Button awayTeamButton = (Button) findViewById(R.id.button_away_team);
+        awayTeamButton.setText(away_team);
+
+        Button playerButton1 = (Button) findViewById(R.id.button_player_1);
+        Button playerButton2 = (Button) findViewById(R.id.button_player_2);
+        Button playerButton3 = (Button) findViewById(R.id.button_player_3);
+        Button playerButton4 = (Button) findViewById(R.id.button_player_4);
+        Button playerButton5 = (Button) findViewById(R.id.button_player_5);
+
+        if(playerList.size() >= 5) {
+            playerButton1.setText(playerList.get(0));
+            playerButton2.setText(playerList.get(1));
+            playerButton3.setText(playerList.get(2));
+            playerButton4.setText(playerList.get(3));
+            playerButton5.setText(playerList.get(4));
+        }
+        else{
+            new AlertDialog.Builder(statRecord.this)
+                    .setTitle(R.string.warning)
+                    .setMessage(R.string.player_not_enough)
+                    .setPositiveButton(R.string.back, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+        }
     }
 }
